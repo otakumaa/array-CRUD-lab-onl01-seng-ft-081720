a18004dc3323883fa761b6e21ca8ee1a61b702be
Bryce Ancheta <bryceancheta@gmail.com>
2020-08-18 23:04:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,19 @@
 def create_an_empty_array
+  []
   
 end
 
 def create_an_array
+  ['dog','fish','bird','reptile']
   
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
